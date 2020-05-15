fb2de32ca44b0de5a9b6d2de021844ab93a176b8
allison_johnson <allison.cuttler@gmail.com>
2020-05-14 19:19:39 -0700

    recipe notes render in a list when manage notes button is clicked

--- src/components/recipeControls.js
+++ src/components/recipeControls.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 import Button from 'react-bootstrap/Button'
+import NotesList from './notesList'
 
 class RecipeControls extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showNotes: this.props.showNotes
+    }
+  }
+
   handleDeleteClick = (e) => {
     this.props.deleteRecipe(parseInt(e.target.id))
+  }
+
+  handleNotesClick = (e) => {
+    this.setState({
+      showNotes: !(this.state.showNotes)
+    })
   }
 
   render() {
@@ -11,7 +25,11 @@ class RecipeControls extends Component {
       <div className="recipe">
         <li style={{padding: '5px'}}>{this.props.recipe.name}
         <Button style={{margin: '5px'}} id={this.props.recipe.id} variant="danger" size="sm" onClick={e => this.handleDeleteClick(e)}>Delete Recipe</Button>{' '}
-        {/* <Button id={recipe.id} variant="warning" size="sm" onClick={e => this.handleNotesClick(e)}>Manage Recipe Notes</Button> */}
+        <Button id={this.props.recipe.id} variant="warning" size="sm" onClick={e => this.handleNotesClick(e)}>Manage Recipe Notes</Button>
+        {this.state.showNotes ? 
+          <NotesList notes={this.props.recipe.notes.filter(note => note.recipe_id === this.props.recipe.id)} /> 
+          : null 
+        }
         </li>
       </div>
     )
